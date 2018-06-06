845c09f75cff43d675cb322a4347e222bb19bcf4
Angai <35431381+naruto6404@users.noreply.github.com>
2018-06-07 01:19:54 +0800

    Add files via upload

---
+++ LINE_LIVE_DOWNLOADER/LINE_LIVE_DOWNLOADER/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace LINE_LIVE_DOWNLOADER
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(12, 331);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(10, 4);
+            this.listBox1.TabIndex = 5;
+            this.listBox1.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("新細明體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox1.Location = new System.Drawing.Point(4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(404, 18);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 81);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(260, 260);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(28)))), ((int)(((byte)(50)))));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Enabled = false;
+            this.textBox2.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox2.ForeColor = System.Drawing.Color.White;
+            this.textBox2.Location = new System.Drawing.Point(278, 96);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(399, 68);
+            this.textBox2.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::LINE_LIVE_DOWNLOADER.Properties.Resources.LOGO;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(148, 75);
+            this.panel1.TabIndex = 6;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(35)))));
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Location = new System.Drawing.Point(147, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(542, 75);
+            this.panel2.TabIndex = 7;
+            this.panel2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Controls.Add(this.textBox1);
+            this.panel3.Controls.Add(this.button2);
+            this.panel3.Location = new System.Drawing.Point(10, 21);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(465, 26);
+            this.panel3.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(408, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(56, 26);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "搜尋";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(35)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Maroon;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(481, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(61, 28);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "╳";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(69)))), ((int)(((byte)(121)))));
+            this.comboBox1.Enabled = false;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.comboBox1.ForeColor = System.Drawing.Color.White;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(483, 185);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(185, 24);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(412, 239);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(256, 44);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "下載";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(291, 312);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(377, 23);
+            this.progressBar1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(28)))), ((int)(((byte)(50)))));
+            this.ClientSize = new System.Drawing.Size(689, 353);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listBox1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ LINE_LIVE_DOWNLOADER/LINE_LIVE_DOWNLOADER/Form1.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Threading;
+
+namespace LINE_LIVE_DOWNLOADER
+{
+    public partial class Form1 : Form
+    {
+        Downloader Downloader1 = new Downloader();
+        Thread T1;
+
+        int m_x, m_y;
+        Boolean drag;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Downloader1.listBox1 = listBox1;
+            Downloader1.pictureBox1 = pictureBox1;
+            Downloader1.textBox2 = textBox2;
+            Downloader1.comboBox1 = comboBox1;
+            Downloader1.progressBar1 = progressBar1;
+            Downloader1.button2 = button2;
+            Downloader1.button3 = button3;
+            Downloader1.textBox1 = textBox1;
+            T1 = new Thread(Downloader1.Download);
+            T1.Start();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            T1.Abort();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (textBox1.Text != "" && e.KeyChar == System.Convert.ToChar(13))
+            {
+                Downloader1.Url = textBox1.Text;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                Downloader1.Url = textBox1.Text;
+            }
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                drag = true;
+                m_x = e.X;
+                m_y = e.Y;
+            }
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (drag) {
+                this.Left += e.X - m_x;
+                this.Top += e.Y - m_y;
+                panel1.Cursor = Cursors.SizeAll;
+                panel2.Cursor = Cursors.SizeAll;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //MessageBox.Show(Downloader1.R[comboBox1.SelectedIndex].Url);
+            if (comboBox1.SelectedIndex != -1)
+            {
+                if (comboBox1.SelectedIndex == 1)
+                {
+                    button3.Text = "下載　" + Downloader1.R[comboBox1.SelectedIndex].Name + "　MP3";
+                }
+                else
+                {
+                    button3.Text = "下載　" + Downloader1.R[comboBox1.SelectedIndex].Name + "p　MP4";
+                }
+                button3.Enabled = true;
+            }
+            else {
+                button3.Enabled = false;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Downloader1.SelectedIndex = comboBox1.SelectedIndex;
+            string Filename_temp;
+            if (comboBox1.SelectedIndex == 1)
+            {
+                saveFileDialog1.FileName = textBox2.Text + ".mp3";
+            }
+            else
+            {
+                saveFileDialog1.FileName = textBox2.Text + "_" + Downloader1.R[comboBox1.SelectedIndex].Name + ".mp4";
+            }
+            Filename_temp = saveFileDialog1.FileName;
+            saveFileDialog1.InitialDirectory = Environment.CurrentDirectory;
+            while (saveFileDialog1.FileName == Filename_temp) {
+                saveFileDialog1.ShowDialog();
+            }
+            //MessageBox.Show(saveFileDialog1.FileName);
+            Downloader1.FileName = saveFileDialog1.FileName;
+            button3.Enabled = false;
+            comboBox1.Enabled = false;
+            textBox1.Enabled = false;
+            button2.Enabled = false;
+            Downloader1.Downloading = true;
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                drag = false;
+
+                if(this.Left < 0)
+                {
+                    this.Left = 0;
+                }
+                else if(this.Left+this.Width > Screen.PrimaryScreen.WorkingArea.Width)
+                {
+                    this.Left = Screen.PrimaryScreen.WorkingArea.Width - this.Width ;
+                }
+
+                if (this.Top < 0)
+                {
+                    this.Top = 0;
+                }
+                else if (this.Top + this.Height > Screen.PrimaryScreen.WorkingArea.Height)
+                {
+                    this.Top = Screen.PrimaryScreen.WorkingArea.Height - this.Height;
+                }
+                panel1.Cursor = Cursors.Default;
+                panel2.Cursor = Cursors.Default;
+            }
+        }
+    }
+
+    public class Resolution {
+        private String _Name = "";
+        private String _Url = "";
+
+        public String Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                _Name = value;
+            }
+        }
+
+        public String Url
+        {
+            get
+            {
+                return _Url;
+            }
+            set
+            {
+                _Url = value;
+            }
+        }
+
+        public void Setting(String Name, String Url) {
+            _Name = Name;
+            _Url = Url;
+        }
+    }
+
+    class Downloader {
+        WebClient WebClient1 = new WebClient();
+        public String Url = "";
+        String[] Splitter = { "lsaPath&quot;:&quot;live/pba/" };
+        String[] Splitter2 = { " : " };
+        public ListBox listBox1 = new ListBox();
+        public TextBox textBox1 = new TextBox();
+        public TextBox textBox2 = new TextBox();
+        public PictureBox pictureBox1 = new PictureBox();
+        public ComboBox comboBox1 = new ComboBox();
+        public ProgressBar progressBar1 = new ProgressBar();
+        public Button button2 = new Button();
+        public Button button3 = new Button();
+
+        public Resolution[] R = new Resolution[6];
+
+        public Boolean Downloading = false;
+        public int SelectedIndex;
+        public string FileName;
+
+        public void Download() {
+            while (true) {
+                if (Downloading)
+                {
+                    string m3u8 = System.Text.Encoding.UTF8.GetString(WebClient1.DownloadData(R[SelectedIndex].Url));
+                    string Url_temp = R[SelectedIndex].Url.Replace("m3u8","");
+                    m3u8 = m3u8.Split(',')[m3u8.Split(',').Length - 1];
+                    int last_count = int.Parse(m3u8.Split('.')[1]);
+                    string download_file_name = "";
+                    System.IO.BinaryWriter BW = new System.IO.BinaryWriter(System.IO.File.OpenWrite(FileName));
+                    for (int i = 0; i <= last_count; i++) {
+                        progressBar1.Value = (100* i / last_count) ;
+                        textBox2.Text ="Downloading " + (100 * i / last_count) + " % ("+ i + "/" + last_count +")";
+                        if (i < 10)
+                        {
+                            download_file_name = Url_temp + "0000" + i + ".ts";                        }
+                        else if (i < 100)
+                        {
+                            download_file_name = Url_temp + "000" + i + ".ts";
+                        }
+                        else if (i < 1000)
+                        {
+                            download_file_name = Url_temp + "00" + i + ".ts";
+                        }
+                        else if (i < 10000)
+                        {
+                            download_file_name = Url_temp + "0" + i + ".ts";
+                        }
+                        else {
+                            download_file_name = Url_temp + i + ".ts";
+                        }
+                        BW.Write(WebClient1.DownloadData(download_file_name));
+                        //progressBar1.Value += (i / last_count * 50);
+                    }
+                    BW.Flush();
+                    BW.Close();
+                    textBox1.Enabled = true;
+                    button2.Enabled = true;
+                    MessageBox.Show(FileName + " 下載完成");
+                    Downloading = false;
+                }
+                else {
+                    if (Url != "")
+                    {
+                        pictureBox1.Image = Properties.Resources.loading;
+                        listBox1.Items.Clear();
+                        comboBox1.Items.Clear();
+                        try
+                        {
+                            //Application.DoEvents();
+                            //richTextBox1.Text += "正在下載資料...\n";
+                            string temp = System.Text.Encoding.UTF8.GetString(WebClient1.DownloadData(Url));
+                            string temp2 = temp.Substring(temp.IndexOf("<title>") + "<title>".Length, temp.IndexOf("</title>") - temp.IndexOf("<title>") - "<title>".Length);
+                            textBox2.Text = temp2.Substring(0, temp2.IndexOf(" - "));
+                            //richTextBox1.Text += "正在解析資料...\n";
+                            temp = temp.Split(Splitter, StringSplitOptions.RemoveEmptyEntries)[temp.Split(Splitter, StringSplitOptions.RemoveEmptyEntries).Length - 1];
+                            temp = temp.Substring(0, temp.IndexOf("&quot"));
+                            //richTextBox1.Text += "正在下載資料...\n";
+                            //richTextBox1.Text += "https://lssapi.line-apps.com/v1/vod/playInfo?contentId=live/pba/" + temp + "&scheme=https&\n";
+                            listBox1.Items.AddRange(System.Text.Encoding.UTF8.GetString(WebClient1.DownloadData("https://lssapi.line-apps.com/v1/vod/playInfo?contentId=live/pba/" + temp + "&scheme=https&")).Replace("\"", "").Split(','));
+                            temp = listBox1.Items[8].ToString().Split(Splitter2, StringSplitOptions.RemoveEmptyEntries)[1];
+                            pictureBox1.Load(temp.Replace(temp.Split('/')[temp.Split('/').Length - 1], "f375x375"));
+                            comboBox1.Enabled = true;
+                            for (int i = 2; i < 9; i++)
+                            {
+                                string[] Rtemp = listBox1.Items[i].ToString().Split(Splitter2, StringSplitOptions.RemoveEmptyEntries);
+                                R[i - 2] = new Resolution();
+                                R[i - 2].Setting(Rtemp[0].Replace(System.Convert.ToChar(10) + "", "").Trim(), Rtemp[1].Substring(0, Rtemp[1].IndexOf("m3u8") + 4));
+                                comboBox1.Items.Add(Rtemp[0]);
+                            }
+                        }
+                        catch (Exception)
+                        {
+                            //throw;
+                        }
+                        Url = "";
+                    }
+                }
+            }
+        }
+    }
+}
